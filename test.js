766a844afebd2cdfee2e4a30dc508b2a248fd6f0
Jirka <del@hs-furtwangen.de>
2016-12-10 17:23:36 +0100

    Added canvas and drawing

--- test.js
+++ test.js
@@ -1,2 +1,9 @@
-alert("Hi there!");
+window.addEventListener("load", init);
+function init(_event) {
+    var canvas = document.getElementsByTagName("canvas")[0];
+    var crc = canvas.getContext("2d");
+    console.log(crc);
+    crc.fillStyle = "red";
+    crc.fillRect(0, 0, canvas.width, canvas.height);
+}
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,1 +1,10 @@
-alert("Hi there!");
+window.addEventListener("load", init);
+
+function init(_event: Event): void {
+    var canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
+    var crc: CanvasRenderingContext2D = canvas.getContext("2d");
+    console.log(crc);
+
+    crc.fillStyle = "red";
+    crc.fillRect(0, 0, canvas.width, canvas.height);
+}
